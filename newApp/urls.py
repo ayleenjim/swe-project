937928593a361e7ef21b359a15dc2418e2baf075
Ayleen Jimenez <ayleen.jimenez01@outlook.com>
2024-10-21 15:15:12 -0500

    ADD login

--- newApp/urls.py
+++ newApp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.sites.url),
     path('', views.home, name= 'home'),
+    path('login/', views.login_user, name= 'login'),
+    path('logout/', views.logout_user, name= 'logout'),
 ]
